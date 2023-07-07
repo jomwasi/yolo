07208c38b0d19b2db0a3aeb1db8e65f20c9e5ad3
Joy Mwasi <jomwasi@gmail.com>
2023-07-07 12:31:08 +0300

    add authentication for aws

--- yolo.tf
+++ yolo.tf
@@ -6,3 +6,9 @@ terraform {
     }
   }
 }
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+access_key = "AKIA34WI7EUXZPWCUJFC"
+  secret_key = "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"
